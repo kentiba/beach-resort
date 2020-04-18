663292f55f5e87a7d786ad2488374ab62b617898
Kentiba <kentiba93@gmail.com>
2020-04-18 17:01:29 +0200

    Updates

---
+++ precache-manifest.136b9e2c16fc398f2dd1e7df0f3bafe2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4e2cf08945ef685ec99a483f3af82b3",
+    "url": "/Beach-Resort/index.html"
+  },
+  {
+    "revision": "5d19a4ceac4169770094",
+    "url": "/Beach-Resort/static/css/4.571a5f7d.chunk.css"
+  },
+  {
+    "revision": "dc21a6fffb44ba7d3cae",
+    "url": "/Beach-Resort/static/css/main.de381ec8.chunk.css"
+  },
+  {
+    "revision": "b0441fd4f0916f76a462",
+    "url": "/Beach-Resort/static/js/0.fe1d2c50.chunk.js"
+  },
+  {
+    "revision": "b2c729f0959049653dfd",
+    "url": "/Beach-Resort/static/js/3.9ee404b0.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Beach-Resort/static/js/3.9ee404b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d19a4ceac4169770094",
+    "url": "/Beach-Resort/static/js/4.e281e26a.chunk.js"
+  },
+  {
+    "revision": "276ab1b63c26d56bfcdd",
+    "url": "/Beach-Resort/static/js/5.a5e927d4.chunk.js"
+  },
+  {
+    "revision": "120466ca6627ed41a0c3",
+    "url": "/Beach-Resort/static/js/6.52eadfa8.chunk.js"
+  },
+  {
+    "revision": "c92f1f52286c564a63a5",
+    "url": "/Beach-Resort/static/js/7.75ba3b9a.chunk.js"
+  },
+  {
+    "revision": "dc21a6fffb44ba7d3cae",
+    "url": "/Beach-Resort/static/js/main.8b384801.chunk.js"
+  },
+  {
+    "revision": "c1f00c3738ebecd3fcb2",
+    "url": "/Beach-Resort/static/js/runtime-main.38edc52e.js"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/Beach-Resort/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "c7c76c7a194983fa489c0e1b2d48dd3b",
+    "url": "/Beach-Resort/static/media/logo.c7c76c7a.svg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/Beach-Resort/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/Beach-Resort/static/media/room-2.052c7681.jpeg"
+  }
+]);
