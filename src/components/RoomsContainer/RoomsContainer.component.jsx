05b56d93477823c0e1df35b9b6e337aef2fdd630
Kentiba <kentiba93@gmail.com>
2020-05-02 15:45:44 +0200

    added new background styles, changed styles in notFound page

--- src/components/RoomsContainer/RoomsContainer.component.jsx
+++ src/components/RoomsContainer/RoomsContainer.component.jsx
@@ -3,16 +3,17 @@ import RoomsList from "./RoomsList.component";
 import RoomsFilter from "./RoomsFilter.component";
 import WithSpinner from "../withSpinner/withSpinner.component";
 import { RoomContext } from "../../Context/Context";
+import "./RoomsContainer.styles.css";
 const RoomsContainer = () => {
   let { sortedRooms, loading } = useContext(RoomContext);
 
   return loading ? (
     <WithSpinner />
   ) : (
-    <>
+    <div className="room">
       <RoomsFilter />
       <RoomsList rooms={sortedRooms} />
-    </>
+    </div>
   );
 };
 
--- src/pages/Not-found.component.jsx
+++ src/pages/Not-found.component.jsx
@@ -1,16 +1,22 @@
 import React from "react";
-import StyledHero from "../components/Hero/StyledHero.component";
 import Banner from "../components/Banner/Banner.component";
 import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
-    <StyledHero>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "4rem"
+      }}
+    >
       <Banner title="404" subtitle="page not found">
         <Link to="/beach-resort/" className="btn-primary">
           Back to Home page
         </Link>
       </Banner>
-    </StyledHero>
+    </div>
   );
 };
 
